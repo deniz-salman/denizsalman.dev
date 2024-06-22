23a9de2c46d776b951f813a7cdab7d942218f729
deniz-salman <denizsalman34@icloud.com>
2024-06-22 17:55:18 +0300

    feat: Add mouse parallax effect.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
+import 'package:mouse_parallax/mouse_parallax.dart';
 
 void main(List<String> args) {
   runApp(const App());
@@ -23,31 +24,50 @@ class _AppState extends State<App> {
           textTheme: GoogleFonts.vt323TextTheme(),
         ),
         home: Scaffold(
-            body: Stack(
-          children: [
-            Lottie.asset('assets/bg.json',
-                fit: BoxFit.cover,
-                width: double.infinity,
-                height: double.infinity),
-            Container(
+            body: ParallaxStack(
+          layers: [
+            Lottie.asset(
+              'assets/bg.json',
+              fit: BoxFit.cover,
               width: double.infinity,
               height: double.infinity,
-              color: Colors.black.withOpacity(0.45),
             ),
-            Center(
-              child: SizedBox(
-                width: 500,
-                height: 400,
-                child: AnimatedTextKit(
-                  totalRepeatCount: 1,
-                  animatedTexts: [
-                    TypewriterAnimatedText(
-                      "Hi 👋🏼\nI'm Deniz",
-                      textStyle: GoogleFonts.vt323()
-                          .copyWith(fontSize: 100, color: Colors.white),
-                      speed: const Duration(milliseconds: 200),
-                    ),
-                  ],
+            ParallaxLayer(
+              yRotation: 0.01,
+              xRotation: 0.01,
+              dimensionalOffset: 0.0018,
+              child: Lottie.asset(
+                'assets/bg.json',
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            ),
+            Container(
+              color: Colors.black.withOpacity(0.45),
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            ParallaxLayer(
+              yRotation: 0.1,
+              xRotation: 0.1,
+              enable3d: true,
+              dimensionalOffset: 0.007,
+              child: Center(
+                child: SizedBox(
+                  width: 500,
+                  height: 400,
+                  child: AnimatedTextKit(
+                    totalRepeatCount: 1,
+                    animatedTexts: [
+                      TypewriterAnimatedText(
+                        "Hi 👋🏼\nI'm Deniz",
+                        textStyle: GoogleFonts.vt323()
+                            .copyWith(fontSize: 100, color: Colors.white),
+                        speed: const Duration(milliseconds: 200),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
